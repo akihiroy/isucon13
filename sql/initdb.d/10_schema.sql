e1967d44466da2a5e527b2e2c321edf325d355e8
ShunIto8919 <108620817+ShunIto8919@users.noreply.github.com>
2023-12-23 15:06:07 +0900

    Update 10_schema.sql

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -116,7 +116,7 @@ CREATE TABLE `ng_words` (
   `word` VARCHAR(255) NOT NULL,
   `created_at` BIGINT NOT NULL,
   INDEX index_user_id(user_id),
-  INDEX ng_words_word ON ng_words(`word`)
+  INDEX ng_words_word (word)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ライブ配信に対するリアクション
